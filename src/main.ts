a6ea255992eb9b6ec9033981b4810a2e71d00be4
Guilherme Eduardo Bublitz <gui.e.bublitz@gmail.com>
2024-08-21 15:06:14 -0300

    setting middlewares to use session and views

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
+import * as hbs from 'hbs';
+
+import * as session from 'express-session';
+import flash = require('connect-flash');
+import * as passport from 'passport';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+  hbs.registerPartials(join(__dirname, '..', 'views/partials'));
+  hbs.registerHelper('eq', function (arg1, arg2) {
+    return arg1 === arg2;
+  });
+  hbs.registerHelper('json', function (context) {
+    return JSON.stringify(context);
+  });
+
+  app.use(
+    session({
+      secret: 'nest cats',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(flash());
+
   await app.listen(3000);
 }
 bootstrap();
